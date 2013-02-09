67bc9a7901bb12b4fef560249ac243cfdbff005a
Bryan Drewery <bryan@shatow.net>
2013-02-08 19:38:51 -0600

    Prefer pypy if available

--- start.sh
+++ start.sh
@@ -4,4 +4,9 @@ if ! [ -d data ]; then
   mkdir data
 fi
 cd data/
-exec nice python ../lib/pyborg/pyborg-irc.py
+
+PYTHON=$(which pypy 2>/dev/null)
+if [ -z "$PYTHON" ]; then
+  PYTHON=python
+fi
+exec nice ${PYTHON} ../lib/pyborg/pyborg-irc.py
