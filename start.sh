389f1f36f6ad748b517d5b43acfd8e427cc5ef50
Bryan Drewery <bryan@shatow.net>
2013-04-01 17:25:49 -0500

    Ensure that proper symlinks are installed
    This is needed as .zip file archives do not properly extract
    the symlinks, resulting in:
    
    ERROR !!!!
    ircbot.py and irclib.py not found, please install them
    ( http://python-irclib.sourceforge.net/ )

--- start.sh
+++ start.sh
@@ -7,4 +7,14 @@ PYTHON=$(which pypy 2>/dev/null)
 if [ -z "$PYTHON" ]; then
   PYTHON=python
 fi
+
+# Ensure the proper symlinks are there. Github's .zip archiving
+# does not properly extract them.
+
+if ! [ -L lib/pyborg/irclib.py ]; then
+	rm -f lib/pyborg/irclib.py lib/pyborg/ircbot.py
+	ln -s ../irclib/irclib.py lib/pyborg/irclib.py
+	ln -s ../irclib/ircbot.py lib/pyborg/ircbot.py
+fi
+
 exec nice ${PYTHON} ../lib/pyborg/pyborg-irc.py
